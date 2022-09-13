3dc4da04492d9947da1e9d011b7e41198da5576a
TauhidulHaqueNaime <mdna19@student.bth.se>
2022-09-13 21:50:32 +0200

    Landing page Full project

---
+++ README.md
@@ -0,0 +1,30 @@
+# Landing Page Project
+
+## Project overview
+Project Name: landing Page Project.
+For exercise of Manipulating the DOM.
+### Used Language in the project
+HTML, CSS, JS DOM
+### Project Directory
+=>landingPageProject
+    =>.github
+    =>css
+        =>styles.css
+    =>js
+        =>app.js
+    =>.DS_Store
+    =>CODEOWNERS
+    =>index.html
+    =>lincense
+    =>README.md
+
+## user instruction
+Click on the index.html and it will open the html file in the default browser or drag the index.html file the browser and it will load in the browser window.
+
+## Project covered [Dynamic project]
+
+* creating header navigation links with dynamic DOM manupulation.
+*generating a active class section dynamically .
+*Highlighting the navigation bar to indicate the position of the top section in the viewport using the active class section.
+*smooth scrolling from section to section using the anchor tag of the navigation bar lists.
+*A subscription form with a submit button.
---
+++ js/app.js
@@ -0,0 +1,123 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+ */
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+ */
+// Global variables
+const navBar = document.getElementById('navbar__list');
+const allSection = document.querySelectorAll('section');
+// Smooth scrolling of the document 
+document.documentElement.style.scrollBehavior = "smooth";
+
+// generating the navigation bar with iterating a loop over the unordered list link
+const navigationBarList = () => {
+    let navigationListCreate = '';
+    allSection.forEach(section => {
+        const IdOfSection = section.id;
+        const DataSetOfSection = section.dataset.nav;
+        navigationListCreate = navigationListCreate + `<li><a class="menu__link" href="#${IdOfSection}">${DataSetOfSection}</a></li>`;
+
+    });
+    navBar.innerHTML = navigationListCreate;
+};
+
+navigationBarList();
+
+//function for acknowledgement of the section which is near top of viewport
+
+const IsInViewPort = (section) => {
+    const ifInViewPort = Math.floor(section.getBoundingClientRect().top);
+    return ifInViewPort;
+};
+
+
+// active class section adding to the section and highlighting related section header navigation link
+const ActiveSectionAdd = (condition, section) => {
+    if (condition == true) {
+        section.classList.add('your-active-class');
+        //highlighting related section header navigation link from the active class section
+        IdOfSection = section.id.slice(7, 8) - 1;
+        navBar.childNodes[IdOfSection].style.cssText = 'color: blue; background-color: orange; font-size: 1.5em; border-radius: 5px';
+    };
+};
+
+// active class section removing fro the last active section and removing the highlight mark from the last selected section header navigation link
+const ActiveSectionRemove = (section) => {
+    section.classList.remove('your-active-class');
+    //resetting the highlight mark from the last selected section header navigation link
+    IdOfSection = section.id.slice(7, 8) - 1;
+    navBar.childNodes[IdOfSection].style.cssText = 'background-color:white';
+};
+
+// fetching the active section from the top of the viewport 
+const ActiveSectionInViewPort = () => {
+    allSection.forEach(section => {
+        const IsInViewPortElement = IsInViewPort(section);
+        inviewport = () => IsInViewPortElement <= 200 && IsInViewPortElement > -200;
+        ActiveSectionRemove(section);
+        ActiveSectionAdd(inviewport(), section);
+    });
+};
+
+window.addEventListener('scroll', ActiveSectionInViewPort);
+
+// Scrolling to the desired section by clicking on the navbar list 
+
+const scrollToAnchor = () => {
+
+    const sectionIDLinks = document.querySelectorAll('.navbar__menu a');
+    sectionIDLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            for (let i = 0; i < allSection; i++) {
+                allSection[i].addEventListener('click', sectionScroll(link));
+            }
+        });
+    });
+
+};
+
+scrollToAnchor();
+
+// form submission 
+function message() {
+    var firstName = document.getElementById('firstName');
+    var lastName = document.getElementById('lastName');
+    var email = document.getElementById('email');
+    var msg = document.getElementById('msg');
+    const success = document.getElementById('success');
+    const fail = document.getElementById('fail');
+
+    if (firstName.value === '' || lastName.value === '' || email.value === '' || msg.value === '') {
+        fail.style.display = 'block';
+    } else {
+        setTimeout(() => {
+            firstName.value = '';
+            lastName.value = '';
+            email.value = '';
+            msg.value = '';
+        }, 500);
+
+        success.style.display = 'block';
+    }
+
+
+    setTimeout(() => {
+        fail.style.display = 'none';
+        success.style.display = 'none';
+    }, 5000);
+
+}
